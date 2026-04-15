9233d44ec54707d5bbeaf31a16fdecb39fab8683
fastium <fastium.pro@proton.me>
2026-04-15 13:23:28 +0200

    feat(user-interface): add example

---
+++ src/examples/example_ui.py
@@ -0,0 +1,35 @@
+import random
+import sys
+
+from PySide6 import QtCore, QtGui, QtWidgets
+
+
+class UserInterface(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel(
+            "Hello World", alignment=QtCore.Qt.AlignmentFlag.AlignCenter
+        )
+
+        self.main_layout = QtWidgets.QVBoxLayout(self)
+        self.main_layout.addWidget(self.text)
+        self.main_layout.addWidget(self.button)
+        self.button.clicked.connect(self.magic)
+
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
+
+def main():
+    app = QtWidgets.QApplication([])
+
+    widget = UserInterface()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
