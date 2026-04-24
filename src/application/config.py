f31ee57d4de126c6e471d4e546a499968d6c3400
fastium <fastium.pro@proton.me>
2026-04-24 09:10:33 +0200

    feat(app): add global configuration of hardware

---
+++ src/application/config.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class AppConfig:
+    # Scope
+    scope_range: float = 2.0
+    scope_bandwidth: float = 300e3
+    scope_coupling: str = "ac"
+    scope_trigger: float = 0.4
+    scope_hysteresis: float = 0.1
+    scope_sample_rate: float = 300e3
+    scope_buffer_size: int = 8192
+
+    # Wavegen
+    wavegen_function: str = "triangle"
+    wavegen_frequency: float = 50.0
+    wavegen_amplitude: float = 1.5
+    wavegen_offset: float = 0.0
+
+    # Gain
+    gain_a0: bool = False
+    gain_a1: bool = True
+    gain_a2: bool = False
